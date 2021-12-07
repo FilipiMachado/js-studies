6957fa443d8be7bef693f52dcb04b5f8bc85de8d
Filipi <filipisee@hotmail.com>
2021-12-07 11:36:59 -0300

    some loop tests

--- loops_tests/script.js
+++ loops_tests/script.js
@@ -1,7 +1,24 @@
-const names = [["0"], ["1"], ["2"], ["3"], ["4"]];
+const names = [
+  ["0", "a", "I"],
+  ["1", "b", "II"],
+  ["2", "c", "III"],
+  ["3", "d", "IV"],
+  ["4", "e", "V"],
+];
 
-for (let i = 0; i < 5; i++) {
-  console.log(`Loop ${i + 1} times`);
+/*  for (let i = 0; i < names.length; i++) {
+  console.log(`Loop ${names.forEach()} times`);
+} 
+
+ for (name of names) {
+    console.log(names[])
+}
+  */
+
+let rows = names.length;
+
+for (let i = 0; i < rows; i++) {
+    
 }
 
-console.log(names)
+console.log(rows);
