48887c3e9e410926a848b78579101ddec37a301d
Filipi <filipisee@hotmail.com>
2021-12-07 11:42:07 -0300

    some loop tests

--- loops_tests/script.js
+++ loops_tests/script.js
@@ -1,11 +1,3 @@
-const names = [
-  ["0", "a", "I"],
-  ["1", "b", "II"],
-  ["2", "c", "III"],
-  ["3", "d", "IV"],
-  ["4", "e", "V"],
-];
-
 /*  for (let i = 0; i < names.length; i++) {
   console.log(`Loop ${names.forEach()} times`);
 } 
@@ -15,10 +7,19 @@ const names = [
 }
   */
 
+const names = [
+  ["0", "a", "I"],
+  ["1", "b", "II"],
+  ["2", "c", "III"],
+  ["3", "d", "IV"],
+  ["4", "e", "V"],
+];
+
 let rows = names.length;
 
 for (let i = 0; i < rows; i++) {
-    
+  let items = i < names[i].length;
+  console.log(i, items);
 }
 
-console.log(rows);
+//console.log(rows);
