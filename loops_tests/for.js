e9328206937b8ca7b9ee77b16d2aecc4c5420128
Filipi <filipisee@hotmail.com>
2021-12-11 10:10:57 -0300

    some tests

--- loops_tests/for.js
+++ loops_tests/for.js
@@ -141,4 +141,5 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // ---- For Structure
 // -->Starting point
 // -->Condition for when your loop should end
-// Increment or decrement || continues up or down
+// --Increment or decrement || continues up or down
+
