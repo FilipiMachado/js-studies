0ceb244826bfa07859ecd350360f649ad6b88bc6
Filipi <filipisee@hotmail.com>
2021-12-11 14:06:37 -0300

    some function tests

---
+++ function_callback/script.js
@@ -0,0 +1,25 @@
+/* const elevarQuadrado = (num) => {
+    console.log(num * num)
+}
+
+ */
+
+/* const saudar = (horaAtual) => {
+  if (horaAtual <= 12) {
+    console.log("Bom Dia!");
+  } else if (horaAtual <= 18) {
+    console.log("Boa Tarde!");
+  } else {
+    console.log("Boa Noite!");
+  }
+};
+
+saudar() */
+
+function criarNome(nome) {
+  return nome = "Fil";
+}
+
+const pessoa = criarNome();
+
+console.log(pessoa)
